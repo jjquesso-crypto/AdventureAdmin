e62e93527c1c32e1b57ddc1d026bfa6b506d402a
juana jael gomez <juanabruno118@gmail.com>
2026-03-26 21:35:17 -0400

    Agrega servicios 1

---
+++ AdventureAdmin.Ui/Services/CustomerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureAdmin.Ui.Services
+{
+    internal class CustomerService
+    {
+    }
+}
---
+++ AdventureAdmin.Ui/Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventureAdmin.Ui.Services;
+
+public class ProductService : Aplicada1.Core.IService<Product, int>
+{
+    public Task<Product?> Buscar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Eliminar(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Product>> GetList(System.Linq.Expressions.Expression<Func<Product, bool>> criterio)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Guardar(Product entidad)
+    {
+        throw new NotImplementedException();
+    }
+}
